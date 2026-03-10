7689114b05306fe0e04cbb664f834a5afac06253
Abhishek Kumar <youremail@gmail.com>
2026-03-10 22:45:21 +0530

    this is main branch file

--- Rivise.java
+++ Rivise.java
@@ -3,6 +3,7 @@ public class Rivise {
     public static void main(String[] args) {
         
         System.out.println("this is coder branch");
+        System.out.println("this is main branch");
     }
     
 }
