9e87a2728b8463c7c08b8b1319b405e2e4ad3b82
Abhishek Kumar <youremail@gmail.com>
2026-03-10 22:25:32 +0530

    coder branch

--- Rivise.java
+++ Rivise.java
@@ -2,10 +2,7 @@
 public class Rivise {
     public static void main(String[] args) {
         
-        System.out.println("Hello I am Coder");
-        System.out.println("Hii i am abhishek");
-    System.out.println("hii world");
-        System.out.println("I am abhishek the coder");
+        System.out.println("this is coder branch");
     }
     
 }
