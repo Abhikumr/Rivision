3ad37ed9ab6c2d3bb0b99c66c7a454314d0e9c9f
Abhishek Kumar <youremail@gmail.com>
2026-03-10 22:22:24 +0530

    from main1

--- Rivise.java
+++ Rivise.java
@@ -1,8 +1,10 @@
+
 public class Rivise {
     public static void main(String[] args) {
         
         System.out.println("Hello I am Coder");
         System.out.println("Hii i am abhishek");
-    }
+    System.out.println("hii world");
     
+}
 }
